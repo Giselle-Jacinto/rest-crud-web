f8adb6edef106f612a728dcb417460f8f4fa13b9
Giselle jacinto <gisellejacinto1219@gmail.com>
2023-12-13 11:31:32 -0300

    Comentários do código

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,114 @@
 "use client";
-import { useState } from "react";
+import { log } from "console";
+import { useState, useEffect, use } from "react";
 import { api } from "../../services/api";
-
-const initialItens = [
-  { id: 1, nome: "Banana" },
-  { id: 2, nome: "Uva" },
-];
-
+//representa tipo elemento existente no array item
+interface Product {
+  id: number;
+  nome: string;
+}
+//função retorna a função/ roda o codigo
 export default function Home() {
+  const [loading, setLoading] = useState(false);
   const [textInput, setTextInput] = useState("");
-  const [itens, setItens] = useState(initialItens);
+  const [items, setItens] = useState<Product[]>([]);
+  // const data = {nome: textInput};
 
-  async function handleClick() {
-    const response = await api.get("/produtos");
-    console.log(response);
-    setItens(response.data);
+  /**
+ * Função responsável por carregar itens no sistema.
+ *
+ * Esta função realiza as operações necessárias para carregar
+ * e exibir os itens no sistema. Pode incluir chamadas a APIs,
+ * manipulação do estado da aplicação, etc.
+ *
+ * @param {TipoDoParametro} nomeDoParametro - Descrição do parâmetro, se houver.
+ * @returns {TipoDoRetorno} - Descrição do que a função retorna, se houver.
+ *
+ * Exemplo de uso:
+ * loadingitens();
+ */
 
-    // const response = await fetch("http://192.168.68.154:3000/produtos");
-    // const produtos = await response.json();
-    // console.log(produtos);
+  //quando ocorrer um evento qualquer a função loadingitens vai rodar
+  useEffect(() => {
+    loadItems();
+  }, []);
+//demostração comofunciona
+  useEffect(() => {
+    console.log("O código está passando por aqui");
+  }, [textInput]);
+
+/**
+ * Função assíncrona responsável por carregar itens.
+ *
+ * Esta função realiza operações assíncronas para buscar e carregar
+ * itens, como chamadas a APIs ou operações de leitura assíncronas.
+ *
+ * @param {TipoDoParametro} nomeDoParametro - Descrição do parâmetro, se houver.
+ * @returns {Promise<TipoDoRetorno>} - Promessa que representa o resultado do carregamento.
+ *
+ * Exemplo de uso:
+ * const items = await loadItems(parametro);
+ */
+  async function loadItems() {
+    setLoading(true);
+    setTimeout(async()=>{
+      try {
+        const response = await api.get("/produtos");
+        console.log(response);
+        setItens(response.data);
+      } catch (error) {
+        console.log("Error: ", error);
+      }
+      finally {
+        setLoading(false);
+      }
+    }, 2000)
+    
   }
+//essa função serve para add um elemento na lista do servidor
+/**
+ * Função assíncrona responsável por carregar itens.
+ *
+ * Esta função realiza operações assíncronas para buscar e carregar
+ * itens, como chamadas a APIs ou operações de leitura assíncronas.
+ *
+ * @param {TipoDoParametro} nomeDoParametro - Descrição do parâmetro, se houver.
+ * @returns {Promise<TipoDoRetorno>} - Promessa que representa o resultado do carregamento.
+ *
+ * Exemplo de uso:
+ * const items = await loadItems(parametro);
+ */
+
 
   async function handleAddItem() {
-    // console.log(textInput);
+    console.log(textInput);
     const data = { nome: textInput };
+    /**
+ * Função assíncrona responsável por lidar com a adição de um novo item.
+ *
+ * Esta função realiza operações assíncronas relacionadas à adição de um novo item,
+ * como interações com APIs, validações e atualizações de estado.
+ *
+ * @param {TipoDoParametro} parametro - Descrição do parâmetro, se houver.
+ * @returns {Promise<TipoDoRetorno>} - Promessa que representa o resultado da operação.
+ *
+ * Exemplo de uso:
+ * const resultado = await handleAddItem(parametro);
+ */
 
     try {
-      const response = await fetch("http://192.168.68.154:3000/produtos", {
-        method: "POST", // or 'PUT'
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+      const response = await api.post("/produtos", data);
 
-      const result = await response.json();
-      console.log("Success:", result);
+      loadItems();
+      console.log("Sucess:", response);
     } catch (error) {
-      console.error("Error:", error);
-      alert("Ocorreu um erro");
-    }
+      console.log("Error:", error);
+    } 
+  }
+//essa função serve para retornar o id do elemento especifico no console
+
+  function handleDeleteItem(idemId: number) {
+    console.log(idemId);
   }
 
   return (
@@ -47,16 +116,19 @@ export default function Home() {
       <div style={{ marginBottom: 10 }}>
         <input
           onChange={(e) => setTextInput(e.target.value)}
-          placeholder="Digite o seu texto aqui..."
+          type="text"
+          placeholder="Digite seu texto aqui..."
         />
         <button onClick={handleAddItem}>Enviar</button>
       </div>
+      <span>{loading && "Carregando.."}</span>
 
-      <button onClick={handleClick}>Buscar informação no servidor</button>
-
-      <ul>
-        {itens.map((item) => (
-          <li key={item.id}>{item.nome}</li>
+      <ul> {/*retorna os elementos do servidor  */}
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.nome}
+            <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+          </li>
         ))}
       </ul>
     </main>
